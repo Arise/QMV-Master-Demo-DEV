9d53694546565cac3d3cb80c48438cf31b365303
Emilio Islas <milio11@yahoo.com>
2017-01-01 18:27:26 -0800

    Update
    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+const gulp = require('gulp')
+const fs   = require('fs')
+const path = require('path')
+
+const docOutput = '../quxios.github.io/data'
+const rmwOutput = './'
+const pluginsPath   = 'js/plugins/'
+const repo = 'https://github.com/quxios/Quasi-MV-Master-Demo/tree/master/js/plugins'
+
+const ignore = ['QBase.js']
+const pluginFiles  = fs.readdirSync(pluginsPath).filter((name) => {
+  return ignore.indexOf(name) === -1
+})
+
+class Plugin {
+  static get(pluginName) {
+    const file   = fs.readFileSync(pluginsPath + pluginName, 'utf8')
+    const header = this.getHeader(file)
+    let plugin   = {
+      name: pluginName.replace('.js', ''),
+      version:  this.getVersion(header),
+      download: this.getParam('download', header),
+      requires: this.getParam('requires', header),
+      tags: this.getParam('tags', header),
+      about: '', // first section of help
+      help: ''
+    }
+    const help = this.getHelp(header)
+    if (help) {
+      plugin.about = help.shift()
+      plugin.help  = help.join('\n')
+    }
+    if (!plugin.download) {
+      plugin.download = `${repo}/${pluginName}`
+    }
+    return plugin;
+  }
+  static getHeader(file) {
+    let match = /\/\*\:([\s\S]*?)\*\//i.exec(file)
+    if (match) {
+      return match[1]
+    }
+    return ''
+  }
+  static getVersion(header) {
+    let match = /Version (\d+.\d+.\d+)/i.exec(header)
+    if (match) {
+      return match[1]
+    }
+    return null
+  }
+  static getParam(param, header) {
+    const regex = new RegExp(`@${param}(.*)`, 'i')
+    const match = regex.exec(header)
+    if (match) {
+      return match[1]
+    }
+    return ''
+  }
+  static getHelp(header) {
+    let help = /@help([\s\S]*?)(\@|\*\/)/.exec(header)
+    if (help) {
+      help = help[1].replace(/^ \*( |)/gm, '')
+      help = help.replace(/\-{70,90}/g, '')
+      help = help.replace(/\={70,90}(\n|\n\r|\r|\r\n)\#\#/g, '--SECTION--\n##')
+      help = help.replace(/\={70,90}/g, '')
+      let sections = help.split('--SECTION--')
+      return sections.filter((s) => {
+        return s.includes('##')
+      })
+    }
+    return null
+  }
+}
+
+gulp.task('docs', function() {
+  let plugins = []
+  let tags = {}
+  pluginFiles.forEach((pluginName) => {
+    if (/^Q/.test(pluginName)) {
+      const plugin = Plugin.get(pluginName);
+      if (plugin.tags) {
+        var newTags = plugin.tags.split(',').map((s) => { return s.trim() })
+        newTags.forEach((tag) => {
+          if (tags[tag]) {
+            tags[tag]++
+          } else {
+            tags[tag] = 1
+          }
+        })
+      }
+      plugins.push(plugin)
+    }
+  })
+  fs.writeFileSync(path.join(docOutput, 'plugins.json'), JSON.stringify(plugins, null, 2))
+  fs.writeFileSync(path.join(docOutput, 'tags.json'), JSON.stringify(tags))
+});
+
+gulp.task('rmw', function() {
+  let text = '';
+  pluginFiles.forEach((pluginName) => {
+    if (/^Q/.test(pluginName)) {
+      const plugin = Plugin.get(pluginName);
+      text += `${plugin.name} - v${plugin.version}\n`;
+      text += `Download - ${plugin.download}\n`
+      text += `${plugin.about}\n\n`;
+    }
+  })
+  fs.writeFileSync(path.join(rmwOutput, 'rmw.txt'), text)
+});
