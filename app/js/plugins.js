579e26b8f30ebdfe545958149a3667d9f198a177
Emilio Islas <exinyo@gmail.com>
2017-03-15 17:26:44 -0700

    updates
    map changes

--- app/js/plugins.js
+++ app/js/plugins.js
@@ -13,18 +13,18 @@ var $plugins =
 {"name":"========================","status":false,"description":"","parameters":{}},
 {"name":"QTouch","status":false,"description":"<QTouch>\nBetter mouse handling for MV","parameters":{"Mouse Decay":"60","Default Cursor":"icon/cursor-default.png","Pointer Cursor":"icon/cursor-pointer.png"}},
 {"name":"========================","status":false,"description":"","parameters":{}},
-{"name":"QInput","status":true,"description":"<QInput>\r\nAdds additional keys to Input class, and allows remapping keys.","parameters":{"Threshold":"0.5","========":"","Ok":"#enter, #space, #z, $A","Escape / Cancel":"#esc, #insert, #x, #num0, $B","Menu":"$Y, #esc","Shift":"#shift, #cancel, $X","Control":"#ctrl, #alt","Tab":"#tab","Pageup":"#pageup, #q, $L1","Pagedown":"#pagedown, #w, $R1","Left":"#left, #num4, $LEFT, #a","Right":"#right, #num6, $RIGHT, #d","Up":"#up, #num8, $UP, #w","Down":"#down, #num2, $DOWN, #s","Debug":"#f9","===========":"","FPS":"f2","Streched":"f3","FullScreen":"f4","Restart":"f5","Console":"f8"}},
+{"name":"QInput","status":true,"description":"<QInput>\r\nAdds additional keys to Input class, and allows remapping keys.","parameters":{"Threshold":"0.1","========":"","Ok":"#enter, #space, #z, $A","Escape / Cancel":"#esc, #insert, #x, #num0, $B","Menu":"$Y, #esc","Shift":"#shift, #cancel, $X","Control":"#ctrl, #alt","Tab":"#tab","Pageup":"#pageup, #q, $L1","Pagedown":"#pagedown, #w, $R1","Left":"#left, #num4, $LEFT, #a","Right":"#right, #num6, $RIGHT, #d","Up":"#up, #num8, $UP, #w","Down":"#down, #num2, $DOWN, #s","Debug":"#f9","===========":"","FPS":"f2","Streched":"f3","FullScreen":"f4","Restart":"f5","Console":"f8"}},
 {"name":"QInputRemap","status":false,"description":"<QInputRemap>\r\nQuasi Input Addon: Adds Key remapping to Options menu","parameters":{"Hide Keys":"","Disable Keys":"ok, escape","===========":"","Vocab: Ok":"Action","Vocab: Escape":"Cancel","Vocab: Shift":"Run","Vocab: Control":"Control","Vocab: Tab":"Tab","Vocab: Pageup":"Next","Vocab: Pagedown":"Prev","Vocab: Up":"Up","Vocab: Down":"Down","Vocab: Left":"Left","Vocab: Right":"Right"}},
 {"name":"QNameInput","status":true,"description":"<QNameInput>\r\nQuasi Input addon: Adds Keyboard Input to Name Input Scene","parameters":{"Show Window with Keys":"true","Window Width":"480"}},
 {"name":"========================","status":false,"description":"","parameters":{}},
 {"name":"QSprite","status":true,"description":"<QSprite>\r\nLets you configure Spritesheets","parameters":{"File Name Identifier":"%{config}-","Random Idle Interval":"60 300","Use New Adjust":"true"}},
 {"name":"QAudio","status":true,"description":"<QAudio>\r\nFew new audio features","parameters":{"Default Radius":"5","Default Max Volume":"100"}},
-{"name":"QSpeed","status":false,"description":"<QSpeed>\r\nAllows for custom Move speeds and an acceleration effect","parameters":{"Acceleration":"true","Duration":"30","===========":"","Dash Inc":"0.5"}},
+{"name":"QSpeed","status":true,"description":"<QSpeed>\r\nAllows for custom Move speeds and an acceleration effect","parameters":{"Acceleration":"true","Duration":"30","===========":"","Dash Inc":"0.5"}},
 {"name":"QCamera","status":true,"description":"<QCamera>\r\nBetter Camera control","parameters":{"Offset":"0.5","Shift Y":"0","Shift X":"0"}},
 {"name":"QYScale","status":true,"description":"<QYScale>\r\nChange characters scale based off their Y value","parameters":{}},
 {"name":"QMap","status":true,"description":"<QMap>\r\ndesc","parameters":{"":""}},
 {"name":"========================","status":false,"description":"","parameters":{}},
 {"name":"QImport","status":true,"description":"<QImport>\r\ndesc","parameters":{"":""}},
 {"name":"QPathfind","status":true,"description":"<QPathfind>\r\nA* Pathfinding algorithm","parameters":{"Diagonals":"true","Intervals":"1000","Smart Wait":"60","=============":"","Half Opt":"true"}},
-{"name":"QSight","status":true,"description":"<QSight>\r\ndesc","parameters":{"":""}}
+{"name":"QSight","status":true,"description":"<QSight>\r\nReal time line of sight","parameters":{"Show":"true"}}
 ];
