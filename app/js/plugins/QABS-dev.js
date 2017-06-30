e6ec7d8dec906b6e845cffa4a1bf3ca4abe4bb1b
Emilio Islas <exinyo@gmail.com>
2017-06-29 17:30:43 -0700

    updates

--- app/js/plugins/QABS-dev.js
+++ app/js/plugins/QABS-dev.js
@@ -32,13 +32,13 @@ if (!Imported.QMovement || !QPlus.versionCheck(Imported.QMovement, '1.4.0')) {
   throw new Error('Error: QABS requires QMovement 1.4.0 or newer to work.');
 }
 
-Imported.QABS = '1.2.1';
+Imported.QABS = '1.2.2';
 
 //=============================================================================
  /*:
  * @plugindesc <QABS>
  * Action Battle System for QMovement
- * @author Quxios  | Version 1.2.1
+ * @author Quxios  | Version 1.2.2
  *
  * @repo https://github.com/quxios/QABS
  *
